61f7995c407bc5f27421831d315e379ca7612910
xiaomaido <xiaomaido@hotmail.com>
2017-08-02 16:11:05 +0800

    右键菜单

--- ams/src/pages/ContextMenuReact.js
+++ ams/src/pages/ContextMenuReact.js
@@ -4,16 +4,15 @@ import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu"
 
 class ContextMenuReact extends Base{
 	handleClick(e, data) {
-		console.log(JSON.stringify(data))
+		alert(JSON.stringify(data))
 	}
 	render(){
 		const some_data="some_data"
 		return (
 		    <div>
 				<ContextMenuTrigger id="some_unique_identifier">
-					<div className="well">鼠标右击查看菜单选项</div>
+					<div style={{padding:'10px'}} className="well">鼠标右击查看菜单</div>
 				</ContextMenuTrigger>
-
 				<ContextMenu id="some_unique_identifier">
 					<MenuItem data={{some_data}} onClick={this.handleClick}>
 						选项 1
@@ -26,6 +25,7 @@ class ContextMenuReact extends Base{
 					    选项 3
 					</MenuItem>
 				</ContextMenu>
+				<a style={{padding:'10px',color:'#999'}} target="_blank" href="https://www.npmjs.com/package/react-contextmenu">了解更多react-contextmenu</a>
 		    </div>
 		)
 	}
