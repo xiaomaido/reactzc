f4c69abbe0c320f2cbbf5bb2315c89585e46e1d3
zengchao <zengchao@huayun.com>
2017-11-17 16:10:58 +0800

    myprofile

--- ams/src/pages/quyou/My.js
+++ ams/src/pages/quyou/My.js
@@ -1,12 +1,13 @@
 
 export default class Index extends Quyou{
 	render(){
-        const avatar=`https://img.xiaohongshu.com/avatar/59bfafb2b46c5d19b53e851b.jpg@120w_120h_92q_1e_1c_1x.jpg?wm=160&hm=160&q=92`
+        // const avatar=`https://img.xiaohongshu.com/avatar/59bfafb2b46c5d19b53e851b.jpg@120w_120h_92q_1e_1c_1x.jpg?wm=160&hm=160&q=92`
         return (
             <div className="my">
                 <div className="icon headbox">
                     <div className="icon wave"></div>
-                    <div className="icon avatar" style={{backgroundImage:`url(${avatar})`}}></div>
+                    <div className="icon avatar"></div>
+                    {/* <div className="icon avatar" style={{backgroundImage:`url(${avatar})`}}></div> */}
                     <div className="arrow-box" onClick={this.openPage.bind(this,`/myprofile`)}>
                         <i className="icon" />
                     </div>
--- ams/src/pages/quyou/MyProfile.js
+++ ams/src/pages/quyou/MyProfile.js
@@ -3,6 +3,38 @@ export default class Index extends Quyou{
         const avatar=`https://img.xiaohongshu.com/avatar/59bfafb2b46c5d19b53e851b.jpg@120w_120h_92q_1e_1c_1x.jpg?wm=160&hm=160&q=92`
         return (
             <div className="my-profile">
+                <ul className="link-list">
+                    <li className="avatarbox">
+                        <div className="icon avatar" style={{backgroundImage:`url(${avatar})`}}></div>
+                        <div className="arrow-box">
+                            <span className="icon" />
+                        </div>
+                        <div className="text">上传头像</div>
+                        <div className="thinner-border clearboth"></div>
+                    </li>
+                    <li>
+                        <div className="name">用户名</div>
+                        <div className="arrow-box">
+                            <span className="icon" />
+                        </div>
+                        <div className="thinner-border clearboth"></div>
+                    </li>
+                    <li>
+                        <div className="name">手机号</div>
+                        <div className="arrow-box">
+                            <span className="icon" />
+                        </div>
+                        <div className="text">修改</div>
+                        <div className="thinner-border clearboth"></div>
+                    </li>
+                    <li>
+                        <div className="name">性别</div>
+                        <div className="arrow-box">
+                            <span className="icon" />
+                        </div>
+                        <div className="thinner-border clearboth"></div>
+                    </li>
+                </ul>
             </div>
         )
     }
