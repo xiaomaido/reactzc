54bcd88c64962c51a679568a4152e3c47ea9720b
zengchao <zengchao@huayun.com>
2017-11-17 16:55:12 +0800

    trip

--- ams/src/pages/quyou/Trip.js
+++ ams/src/pages/quyou/Trip.js
@@ -1,9 +1,21 @@
-
+import bar from '../../images/quyou/trip/bar.png'
+import bus from '../../images/quyou/trip/bus.png'
 export default class Index extends Quyou{
 	render(){
         return (
-            <div>
-                123
+            <div className="trip">
+                <div className="top">
+                    <img className="bar" src={bar}  />
+                    <div style={{height:0}}>&nbsp;</div>
+                </div>
+                <div className="banner-box">
+                    <div className="bigsmall">
+                        <div className="big"><span>打造</span>低碳环保出行</div>
+                        <div className="small">公共交通线路时刻一览表</div>
+                    </div>
+                    <img className="bus" src={bus}  />
+                </div>
+                <div style={{height:0}}>&nbsp;</div>
             </div>
         )
     }
