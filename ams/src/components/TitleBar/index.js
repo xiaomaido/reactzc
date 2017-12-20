5c062c79a8bfbe425c497d54e66f2d0287262f6d
xiaomaido <xiaomaido@hotmail.com>
2017-12-20 20:12:40 +0800

    sss

--- ams/src/components/TitleBar/index.js
+++ ams/src/components/TitleBar/index.js
@@ -2,22 +2,29 @@ import './index.scss'
 const Index = (props) => {
     const { location, objTitleBack } = props
     const current = location.pathname.split('/')
-    let backUrl = `/${current[1]}`
-    backUrl = current.length === 3 ? backUrl : objTitleBack[backUrl]
     const { search, query } = props.location
     const _t = query._t || 'EAT'
-    const titleimg = current[1] + _t.toLowerCase()
-    if(backUrl === '/'){
-        if(_t === 'SLEEP') backUrl='/hotel'
-        else if(_t === 'TOUR') backUrl='/trip'
-    }
-    else if(backUrl === '/posthot' || backUrl === '/videohot'){
-        backUrl = backUrl + search
-    }
-    else if(backUrl === '/shophot'){
-        if(_t === 'SLEEP') backUrl='/hotelhot'
-        else if(_t === 'TOUR') backUrl='/triphot'
-    } 
+    let titleimg = current[1] + _t.toLowerCase()
+    let backUrl = ''
+    // window.locationFrom = window._location
+    // if(window.locationFrom && typeof window.locationFrom === 'object'){
+    //     backUrl = window.locationFrom.pathname
+    // }else{
+        backUrl = `/${current[1]}`
+        backUrl = current.length === 3 ? backUrl : objTitleBack[backUrl]
+        if(backUrl === '/'){
+            if(_t === 'SLEEP') backUrl='/hotel'
+            else if(_t === 'TOUR') backUrl='/trip'
+        }
+        else if(backUrl === '/posthot' || backUrl === '/videohot'){
+            backUrl = backUrl + search
+        }
+        else if(backUrl === '/shophot'){
+            if(_t === 'SLEEP') backUrl='/hotelhot'
+            else if(_t === 'TOUR') backUrl='/triphot'
+        }
+    // }
+    console.log(backUrl)
     return (
         <div className="titleBar">
             <div className="box">
